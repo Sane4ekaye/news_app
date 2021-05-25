d600c09d0aaf4e666be4da2d16dc459b1872399c
chikibambsanya <BKyPeHblu456@yandex.ru>
2021-05-25 16:19:04 +0400

    создал проект

---
+++ app/src/main/java/com/chikieblan4ik/newsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chikieblan4ik.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
