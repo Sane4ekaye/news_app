ba65044d4fadb64a86f4d3f1eec239d76d90a4af
MistiJI <spam-mistbtv@yandex.ru>
2021-05-25 16:39:57 +0400

    test

--- app/src/main/java/com/chikieblan4ik/newsapp/MainActivity.kt
+++ app/src/main/java/com/chikieblan4ik/newsapp/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //
 //        connectURL("https://www.express.co.uk/")
     }
 
